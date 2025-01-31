1e33c28054ae1c08885cd84a1cc51e24dbb82d88
StarHidden <StarHidden2846@gmail.com>
2025-01-31 12:15:41 +0800

    test connection

---
+++ entry/src/main/ets/models/MulSensor.ts
@@ -0,0 +1,22 @@
+
+export class MulSensor{
+  private _temperature: string = '';
+
+  public set temperature(value: string) {
+    this._temperature = value;
+  }
+
+  public get temperature(): string {
+    return this._temperature;
+  }
+
+  private _humidity: string = '';
+
+  public set humidity(value: string) {
+    this._humidity = value;
+  }
+
+  public get humidity(): string {
+    return this._humidity;
+  }
+}
